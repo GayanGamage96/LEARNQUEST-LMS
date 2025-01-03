b3193644cce0fb8555c322ebbafceaca7bdfa662
GayanGamage96 <gayangamagercg1996@gmail.com>
2025-01-03 22:20:33 +0530

    intial

---
+++ client/src/app/page.tsx
@@ -0,0 +1,13 @@
+import NonDashboardNavbar from '@/components/NonDashboardNavbar';
+import LandingPage from "@/app/(nondashboard)/landing/page";
+import Footer from '@/components/Footer';
+
+export default function Home() {
+  return <div className="nondashboard-layout">
+    <NonDashboardNavbar/>
+    <main className='nondashboard-layout__main'>
+<LandingPage/>
+    </main>
+    <Footer/>
+  </div>;
+}
